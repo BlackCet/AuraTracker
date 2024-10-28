10706c97a66c9ca463428ab189bc1ea0eab1418a
Astha <asthajaiswal312@gmail.com>
2024-10-28 23:03:39 +0530

    created user routes, controller and model

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,31 @@ require('dotenv').config()
 
 //requre express
 const express = require('express')
+const userRoutes = require('./routes/User')
+const mongoose = require('mongoose')
 
 //create an express app
 const app = express()
 
 //logger middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(`${req.method} request for ${req.url}`)
     next()
 })
 
-//route
-app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to the server'})
-})
+//routes
+app.use('/api/user', userRoutes)
 
-//listen on port
-app.listen(process.env.PORT, () => {
-  console.log(`listening at port ${process.env.PORT}`)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
