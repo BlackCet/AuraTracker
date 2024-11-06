c23c39e971e9aba64a243442d2199083fa5fc831
Astha <asthajaiswal312@gmail.com>
2024-11-06 23:47:45 +0530

    gaol filter

--- frontend/src/pages/AcademicGoals.js
+++ frontend/src/pages/AcademicGoals.js
@@ -4,6 +4,7 @@ import AddGoalForm from '../components/GoalForm';
 
 const Goals = () => {
     const [goals, setGoals] = useState([]);
+    const [filter, setFilter] = useState('All'); // Add state for the filter
 
     useEffect(() => {
         fetchGoals();
@@ -60,14 +61,49 @@ const Goals = () => {
         }
     };
 
+    const filterGoals = () => {
+        if (filter === 'All') {
+            return goals;
+        }
+        return goals.filter(goal => goal.priority === filter);
+    };
+
     return (
-        <div className="app p-4 flex justify-between space-x-4">
-            <div className="w-1/3">
-                <h1 className="text-2xl font-bold mb-4">Academic Goals</h1>
-                <AddGoalForm onAdd={addGoal} />
+        <div className="app p-4 flex flex-col">
+            <div className="flex justify-center mt-4">
+                <button
+                    className={`p-2 mx-2 ${filter === 'All' ? 'bg-teal-500 text-white hover:bg-teal-light' : 'bg-gray-200 hover:bg-teal-light'}`}
+                    onClick={() => setFilter('All')}
+                >
+                    All
+                </button>
+                <button
+                    className={`p-2 mx-2 ${filter === 'Low' ? 'bg-teal-500 text-white hover:bg-teal-light' : 'bg-gray-200 hover:bg-teal-light '}`}
+                    onClick={() => setFilter('Low')}
+                >
+                    Low
+                </button>
+                <button
+                    className={`p-2 mx-2 ${filter === 'Medium' ? 'bg-teal-500 text-white hover:bg-teal-light' : 'bg-gray-200 hover:bg-teal-light'}`}
+                    onClick={() => setFilter('Medium')}
+                >
+                    Medium
+                </button>
+                <button
+                    className={`p-2 mx-2 ${filter === 'High' ? 'bg-teal-500 text-white hover:bg-teal-light' : 'bg-gray-200 hover:bg-teal-light'}`}
+                    onClick={() => setFilter('High')}
+                >
+                    High
+                </button>
             </div>
-            <div className="w-2/3 p-12">
-                <GoalList goals={goals} onUpdate={updateGoal} onDelete={deleteGoal} />
+            <div className="flex justify-between mb-4">
+                <div className="w-1/3 mr-4">
+                    <h1 className="text-2xl font-bold mb-4">Academic Goals</h1>
+                    <AddGoalForm onAdd={addGoal} />
+                </div>
+                <div className="w-2/3 mt-12">
+                    <GoalList goals={filterGoals()} onUpdate={updateGoal} onDelete={deleteGoal} />
+                </div>
             </div>
         </div>
     );
