90330516c349d7efdf1b2f9d8055cae9ac7ca8c7
Anupma <anupma30102005@gmail.com>
2024-11-09 18:47:03 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ import Events from "./pages/Events";
 import About from "./pages/About";
 import Profiles from "./pages/Profiles";
 import Team from "./pages/Team";
-import Timer from "./pages/Timer";
 function App() {
   const { user } = useAuthContext()
   
@@ -33,7 +32,6 @@ function App() {
             <Route path="/schedule" element={user ? <Events/> : <Navigate to="/schedule" />} />
             <Route path="/about" element={<About />} />
             <Route path="/team" element={<Team />} />
-            <Route path="/Virtual Study Rooms" element={user ? <Timer />: <Navigate to="/login" />} />
 
           </Routes>
         </div>
